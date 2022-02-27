ace1e7613bdc3d31b56c7c79b39b0f8c59af36a2
Ли Сергей <sergey.r.lee@gmail.com>
2022-02-27 15:08:26 +0300

    ДЗ № 10 выполнено.

---
+++ src/sky/pro/java/course1/homework10/Author.java
@@ -0,0 +1,39 @@
+package sky.pro.java.course1.homework10;
+
+import java.util.Objects;
+
+public class Author {
+    private final String authorName;
+    private final String authorSurname;
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    @Override
+    public String toString() {
+        return authorName + " " + authorSurname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return authorName.equals(author.authorName) && authorSurname.equals(author.authorSurname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorName, authorSurname);
+    }
+}
